9f9d2158db2bde225c54808bc83130d9c1e2fa24
boucherlfg <boucherlfg@gmail.com>
2025-02-18 11:49:51 -0500

    life potion consumable

--- Assets/Healer.cs
+++ Assets/Healer.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Healer : MonoBehaviour
 {
+    [SerializeField] private bool healOnStart = false;
     [SerializeField] private float healAmount = 50;
     public void Heal()
     {
         var life = FindAnyObjectByType<LifeScript>();
         life.CurrentLife = Mathf.Min(life.CurrentLife + healAmount, life.Life);
+    }
+
+    private void Start()
+    {
+        if (!healOnStart) return;
+        Heal();
+        Destroy(gameObject);
     }
 }
